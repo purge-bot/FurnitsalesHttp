d1faf9134cd2edb08950bf2033380ba895e590e8
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-11-26 02:05:47 +0300

    Создание моделей

---
+++ Furnits/Models/Products/Assortment/Divan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Furnits.Models.Products.Assortment
+{
+    public class Divan
+    {
+    }
+}
