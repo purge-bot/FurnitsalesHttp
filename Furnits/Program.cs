3ca18ed79d5448ef02a7573fc6af83e0452fd2b6
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-11-26 01:46:31 +0300

    Базовые настройки проекта

--- Furnits/Program.cs
+++ Furnits/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,14 +14,14 @@ namespace Furnits
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseDefaultServiceProvider(options =>
+            options.ValidateScopes = false)
+            .Build();
     }
 }
