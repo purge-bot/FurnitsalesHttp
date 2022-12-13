5bd61d19b172dcae7c7994c271d5ec12300432c4
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-12-13 04:44:16 +0300

    Добавление контроллера создания пользователей

---
+++ Furnits/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using Furnits.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Furnits.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Furnits.Controllers
+{
+    [Authorize(Roles = "Admins")]
+    public class AdminController : Controller
+    {
+        private UserManager<User> _userManager;
+
+        public AdminController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public ViewResult Index()
+        {
+            return View(_userManager.Users);
+        }
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
