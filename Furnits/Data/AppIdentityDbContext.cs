c0d8233c602f9dd46a53a6cb89021dddcf7f1cca
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-12-13 04:42:24 +0300

    Добавление класса пользователя

--- Furnits/Data/AppIdentityDbContext.cs
+++ Furnits/Data/AppIdentityDbContext.cs
@@ -1,18 +1,49 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Furnits.Models.Users;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Furnits.Data
 {
-    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+    public class AppIdentityDbContext : IdentityDbContext<User>
     {
         public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
         {
 
+        }
+
+        public static async Task CreateAdminAccount(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            UserManager<User> userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            RoleManager<IdentityRole> roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string userName = configuration["Data:AdminUser:Name"];
+            string email = configuration["Data:AdminUser:Email"];
+            string password = configuration["Data:AdminUser:Password"];
+            string role = configuration["Data:AdminUser:Role"];
+
+            if(await userManager.FindByNameAsync(userName) == null)
+            {
+                if(await roleManager.FindByNameAsync(role) == null)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+
+                User userAdmin = new User() { 
+                    UserName = userName,
+                    Email = email
+                };
+                IdentityResult result = await userManager.CreateAsync(userAdmin, password);
+
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(userAdmin, role);
+                }
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
---
+++ Furnits/Models/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Furnits.Models.Users
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+        }
+
+        public User(string userName) : base(userName)
+        {
+        }
+    }
+}
