d8da57e33c361be7f4539b9bd854b510986b9f9f
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-12-23 02:29:49 +0300

    Обновление таблицы ProductType

--- Furnits/Data/Migrations/20221222232536_UpdateProductTypes.Designer.cs
+++ Furnits/Data/Migrations/20221222232536_UpdateProductTypes.Designer.cs
@@ -2,15 +2,17 @@
 using Furnits.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Furnits.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221222232536_UpdateProductTypes")]
+    partial class UpdateProductTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Furnits/Data/Migrations/20221222232536_UpdateProductTypes.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Furnits.Data.Migrations
+{
+    public partial class UpdateProductTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TypeCode",
+                table: "ProductTypes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductTypes_TypeCode",
+                table: "ProductTypes",
+                column: "TypeCode",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProductTypes_TypeCode",
+                table: "ProductTypes");
+
+            migrationBuilder.DropColumn(
+                name: "TypeCode",
+                table: "ProductTypes");
+        }
+    }
+}
