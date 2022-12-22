72e418ac7f2fb964ea0547926aa692b4f46b17f7
nerovnovaa@gmail.com <nerovnovaa@gmail.com>
2022-12-23 02:30:19 +0300

    Обновление класса ProductType

--- Furnits/Models/Products/ProductType.cs
+++ Furnits/Models/Products/ProductType.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Furnits.Models.Products
 {
+    [Index(nameof(TypeCode), IsUnique = true)]
     public class ProductType
     {
         public int Id { get; set; }
+        public string TypeCode { get; set; }
         public string TypeName { get; set; }
 
         public int ProductArticle { get; set; }
